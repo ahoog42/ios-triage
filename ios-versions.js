fcf927100be426fd080db9e83443f8a26358355f
Andrew Hoog <ahoog@nowsecure.com>
2017-02-13 17:02:47 -0600

    simplist way to track current iOS version

---
+++ ios-versions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// future work would be to query TSSstatus API to grab this info
+// but then the app would hit the interwebs. Could make configurable
+// this would allow us to easily determine latest ios version by device
+// https://api.ineal.me/tss/status
+
+module.exports = {
+  LATEST_IOS_VERSION: '10.2.1'
+};
