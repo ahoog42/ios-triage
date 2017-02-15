ed024f06315f09b2a0682ea5fe0f717287feb7ff
Andrew Hoog <ahoog@nowsecure.com>
2017-02-14 19:05:38 -0600

    temp removed NANDInfo key from com.apple.disk_usage and com.apple.disk_usage.factory until I can address the issue where plist.parse munges the data elements of plists

--- index.js
+++ index.js
@@ -1170,6 +1170,15 @@ function generateReport (dir, diffdir, callback) {
 
       const data = readProcessedJSON(dir, true);
       if (doDiff) {
+        // currently plist parse tries to convert data elements by base64 decoding them
+        // and making into an array of values. This really messes up diff and such so for now
+        // we'll delete these values for our diff comparision. 
+        // FIXME: change plist.parse to not munge the data nodes
+        delete data.device.details["com.apple.disk_usage.factory"].NANDInfo;
+        delete data.device.details["com.apple.disk_usage"].NANDInfo;
+        delete dataRhs.device.details["com.apple.disk_usage.factory"].NANDInfo;
+        delete dataRhs.device.details["com.apple.disk_usage"].NANDInfo;
+
         const diff = deepdiff(data, dataRhs);
         data.diff = diff;
       }
