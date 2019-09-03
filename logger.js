44eb7c9fafdb6c94e6c847355fd01207f285f0d6
Andrew Hoog <ahoog42@gmail.com>
2019-09-03 17:29:40 -0500

    better console logging

--- logger.js
+++ logger.js
@@ -3,21 +3,16 @@
 const winston = require('winston');
 const tsFormat = () => (new Date()).toLocaleTimeString();
 
-const logger = new (winston.Logger)({
+let logger = winston.createLogger({ 
   transports: [
     // colorize the output to the console
     new (winston.transports.Console)({
       timestamp: tsFormat,
       colorize: true,
       level: 'info'
-    })
-// will add later when i figure out how to either update filename from calling js
-//    new (winston.transports.File)({
-//      filename: 'debug.log',
-//      timestamp: tsFormat,
-//      level: 'debug'
-//    })
-  ]
+    }),
+  ],
+  exitOnError: false
 });
 
 module.exports = logger;
